5104e1f101d3889ffe67bb56240ba7ed472756f1
wangxuwen <wangxuwen_cm@deepin.com>
2019-12-30 13:38:20 +0800

    fixed: 在锁屏界面屏蔽Win,ctrl 按键bug(10021)

--- session-widgets/userinputwidget.cpp
+++ session-widgets/userinputwidget.cpp
@@ -389,6 +389,14 @@ bool UserInputWidget::eventFilter(QObject *watched, QEvent *event)
         grabKeyboard();
     }
 
+    if (event->type() == QEvent::KeyPress) {
+        QKeyEvent *key_event = static_cast<QKeyEvent *>(event);
+        if (key_event->modifiers() & (Qt::ControlModifier | Qt::AltModifier | Qt::MetaModifier)) {
+            if ((key_event->modifiers() & Qt::ControlModifier) && key_event->key() == Qt::Key_A) return false;
+            return true;
+        }
+    }
+
     return QWidget::eventFilter(watched, event);
 }
 
